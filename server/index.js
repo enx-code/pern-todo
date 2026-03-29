2e88c22fa616a770ed6968a3fd79087213d72c4b
Enky <enkh6at@yahoo.com>
2026-03-30 00:03:07 +0100

    requiring from /.db pool

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 app.use(cors());
 app.use(express.json());
+// ROUTES
+
+// Create a todo
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
