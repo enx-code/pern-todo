55726a1e00c1dd357d53a4b3a76fe448e4f3df9d
Enky <enkh6at@yahoo.com>
2026-04-01 18:30:37 +0100

    creating routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,21 @@ app.use(express.json());
 // ROUTES
 
 // Create a todo
+app.post('/todos', async (req, res) => {
+    try {
+        console.log(req.body);
+        res.json("Todo created");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo    
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
