3290401da87d919fd51881e9243c0d9f602a4fe3
Enky <enkh6at@yahoo.com>
2026-04-04 19:29:09 +0100

    fixed database

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "password",
+    user: "Enky",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "perntodo"
--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
+
+// Middleware
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //request body is in json format
 
-app.get("/", (req, res) => {
-  res.send("Server is alive");
-});
-
+// create a todo
 app.post("/todos", async (req, res) => {
   try {
-    console.log("BODY RECEIVED:", req.body);
-    res.status(200).json("Todo created");
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+    
+
   } catch (err) {
     console.error("ERROR:", err.message);
     res.status(500).json("Server error");
   }
-});
+})
+
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// ADD THIS:
-setInterval(() => {}, 1000); // keeps Node event loop alive
