de840d8202772587910cd1e231c64738e81af34f
Enky <enkh6at@yahoo.com>
2026-04-08 18:52:12 +0100

    creating form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import './App.css';
-
+import InputTodo from './components/InputTodo';
 function App() {
   return (
     <Fragment>
-      
+      <div className="container">
+        <InputTodo />
+      </div>
     </Fragment>
 
   );
