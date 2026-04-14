fede8beafdcb06266c1696e1f1f3b45e4c9c6744
Enky <enkh6at@yahoo.com>
2026-04-14 16:12:26 +0100

    table added

--- client/src/components/listTodo.js
+++ client/src/components/listTodo.js
@@ -2,9 +2,34 @@ import React, {Fragment} from "react";
 
 const ListTodo = ({ todos }) => {
     return (
-        <Fragment>
-        
-        </Fragment>
+      <Fragment>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john@example.com</td>
+            </tr>
+            <tr>
+              <td>Mary</td>
+              <td>Moe</td>
+              <td>mary@example.com</td>
+            </tr>
+            <tr>
+              <td>July</td>
+              <td>Dooley</td>
+              <td>july@example.com</td>
+            </tr>
+          </tbody>
+        </table>
+      </Fragment>
     );
 }
 
