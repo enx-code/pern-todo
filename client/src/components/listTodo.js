887e429c7ba43922a52df91597855da69ee5d0f4
Enky <enkh6at@yahoo.com>
2026-04-15 17:24:51 +0100

    deleting todos

--- client/src/components/listTodo.js
+++ client/src/components/listTodo.js
@@ -2,6 +2,19 @@ import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodo = () => {
   const [todos, setTodos] = useState([])
+
+  // delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:8000/todos/${id}`, {
+        method: "DELETE"
+      });
+      setTodos(todos.filter(todo => todo.todo_id !== id))
+    } catch (err) {
+      console.error(err.message);
+    }
+  };  
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:8000/todos");
@@ -37,8 +50,8 @@ const ListTodo = () => {
             {todos.map(todo => (
               <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
-                <td>Edit</td>
-                <td>Delete</td>
+                <td><button className="btn btn-warning">Edit</button></td>
+                <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
               </tr>
             ))}
           </tbody>
