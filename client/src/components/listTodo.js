37925b02cd0edca0b217d3f4e7500a7d012a859b
Enky <enkh6at@yahoo.com>
2026-04-15 14:13:46 +0100

    mapping through todos

--- client/src/components/listTodo.js
+++ client/src/components/listTodo.js
@@ -1,32 +1,46 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
-const ListTodo = ({ todos }) => {
+const ListTodo = () => {
+  const [todos, setTodos] = useState([])
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []); 
+
+  console.log(todos)
     return (
       <Fragment>
-        <table class="table">
+        <table className="table mt-5 text-center">
           <thead>
             <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Email</th>
+              <th>Description</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            {/* <tr>
               <td>John</td>
               <td>Doe</td>
               <td>john@example.com</td>
-            </tr>
-            <tr>
-              <td>Mary</td>
-              <td>Moe</td>
-              <td>mary@example.com</td>
-            </tr>
-            <tr>
-              <td>July</td>
-              <td>Dooley</td>
-              <td>july@example.com</td>
-            </tr>
+            </tr> */}
+            {todos.map(todo => (
+              <tr key={todo.todo_id}>
+                <td>{todo.description}</td>
+                <td>Edit</td>
+                <td>Delete</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </Fragment>
